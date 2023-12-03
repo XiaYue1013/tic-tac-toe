83f7f2bed00ef8d887faa8dae9397affe889ed8e
XiaYue <admin@xiayue.tw>
2023-12-04 00:00:08 +0800

    Initial Commit

---
+++ tic-tac-toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tic-tac-toe
+//
+//  Created by Sin-Yu Jhu on 2023/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
